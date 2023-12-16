977fd2c5ed48fc7c4003a76e6d94190725e6996c
Asus <tarikhasan12200@gmail.com>
2023-12-16 01:17:11 -0800

    fixed issue

--- Assets/Scripts/TouchControl.cs
+++ Assets/Scripts/TouchControl.cs
@@ -1,74 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using Gamekit3D;
 using UnityEngine;
 
 public class TouchControl : MonoBehaviour
 {
-    public float holdDuration = 1f; // Adjust the duration as needed
+    private bool isTouching = false;
+    public float holdDuration = 2f; // Adjust the duration as needed
     public Animator _anim;
     private float touchStartTime;
-    public GiftBoxController gf;
     public ToggleVisibility tv;
     public GameObject QuestionMenu;
+    public InteractionTrigger interactionTrigger;
 
     private void Update()
     {
-        // Check for touch input
         if (Input.touchCount > 0)
         {
+            // Get the first touch
             Touch touch = Input.GetTouch(0);
+
+            // Perform raycasting from the touch position
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            RaycastHit hit;
 
             switch (touch.phase)
             {
                 case TouchPhase.Began:
-                    // Check if the touch began on the object
-                    if (IsTouchingObject(touch.position))
+                    // Record touch start time
+                    isTouching = true;
+                    touchStartTime = Time.time;
+                    break;
+
+                case TouchPhase.Moved:
+                    // Check if the touch is still within the same object
+                    if (isTouching && Physics.Raycast(ray, out hit) && hit.transform.CompareTag("giftbox"))
                     {
-                        // Store the start time of the touch
-                        touchStartTime = Time.time;
+                        // Check if touch has been held for at least 2 seconds
+                        if (Time.time - touchStartTime >= 2f)
+                        {
+                            // Call your custom function when a "giftbox" is touched and held for 2 seconds
+                            OnTouchAndHoldStart();
+                        }
                     }
                     break;
 
                 case TouchPhase.Ended:
-                    // Check if the touch duration is exactly holdDuration
-                    if (Time.time - touchStartTime >= holdDuration)
-                    {
-                        // Call the function when the touch ends and the duration is 4f
-                        OnTouchAndHoldStart();
-                        Debug.Log("clicked");
-                        gf.HandleClick();
-                        tv.ToggleImageVisibility(QuestionMenu);
-                        OnTouchAndHoldEnd();
-                    }
-
-                    // Call the function when the touch ends regardless of duration
-                    
+                    // Reset touch state
+                    isTouching = false;
                     break;
             }
         }
     }
 
-    private bool IsTouchingObject(Vector2 touchPosition)
-    {
-        // Cast a ray from the touch position to see if it hits the object
-        Ray ray = Camera.main.ScreenPointToRay(touchPosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit))
-        {
-            return hit.collider.gameObject == gameObject;
-        }
-
-        return false;
-    }
 
     private void OnTouchAndHoldStart()
     {
         _anim.SetTrigger("Open");
-    }
-
-    private void OnTouchAndHoldEnd()
-    {
+        interactionTrigger.enabled = true;
+        tv.ToggleImageVisibility(QuestionMenu);
         _anim.SetTrigger("Pickup");
+
+
     }
+    
 }
